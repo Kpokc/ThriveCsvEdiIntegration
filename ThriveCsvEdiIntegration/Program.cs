4f0ec10476e4a954db664f0269d7066e9aec8d19
Pavel Makarov <pavmakarovs@gmail.com>
2024-08-30 11:47:05 +0100

    Add project files.

---
+++ ThriveCsvEdiIntegration/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetEnv;
+
+namespace ThriveCsvEdiIntegration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Load environment variables from the specified .env file (paths.env)
+                Env.Load("./paths.env");
+
+                string inputFolder = Environment.GetEnvironmentVariable("INPUTPATH");
+
+                string[] files = Directory.GetFiles(inputFolder, "*csv");
+
+                foreach (string file in files) 
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
